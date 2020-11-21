082de48f01da8c0db5407f37a3474a01662b3aaf
srushtipathak <srushtipathak16@gmail.com>
2020-11-21 17:00:01 +0530

    first

---
+++ src/test/java/COM/LTI/AppTest.java
@@ -0,0 +1,21 @@
+package COM.LTI;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("Welcome to LTI");
+        assertTrue( true );
+    }
+}
